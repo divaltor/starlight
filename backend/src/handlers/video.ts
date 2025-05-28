d220d1336410c270523c7474950830e5a447e945
Vlad Vladov <divaltor@pm.me>
2025-05-28 19:42:54 +0300

    Handle too big video error

--- backend/src/handlers/video.ts
+++ backend/src/handlers/video.ts
@@ -1,6 +1,6 @@
 import type { Context } from "@/bot";
 import { downloadVideo } from "@/services/video";
-import { Composer, InputFile } from "grammy";
+import { Composer, GrammyError, InputFile } from "grammy";
 import tmp from "tmp";
 
 const composer = new Composer<Context>();
@@ -19,10 +19,21 @@ feature.on(":text").filter(
 		const videos = await downloadVideo(ctx.msg.text, tempDir.name);
 
 		for (const video of videos) {
-			await ctx.replyWithVideo(new InputFile(video.filePath), {
-				width: video.metadata?.width,
-				height: video.metadata?.height,
-			});
+			try {
+				await ctx.replyWithVideo(new InputFile(video.filePath), {
+					width: video.metadata?.width,
+					height: video.metadata?.height,
+				});
+			} catch (error) {
+				if (error instanceof GrammyError) {
+					if (error.error_code === 413) {
+						await ctx.reply("Video is too large, can't be sent");
+					} else {
+						await ctx.reply("Error downloading video");
+						throw error;
+					}
+				}
+			}
 		}
 
 		tempDir.removeCallback();
