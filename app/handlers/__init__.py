a84064249369595ff9fd7c69c94a14a41f2d6c98
Vlad Vladov <divaltor@pm.me>
2025-05-29 19:42:09 +0300

    Completely remove Python

--- app/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher
-
-from app.handlers import video
-
-
-def setup_handlers(dp: Dispatcher) -> None:
-    dp.include_router(video.router)
--- app/services/models/base.py
+++
@@ -1,9 +0,0 @@
-from typing import Any, Self
-
-from pydantic import BaseModel as PydanticBaseModel
-
-
-class BaseModel(PydanticBaseModel):
-    @classmethod
-    def from_twitter_response(cls, data: Any) -> Self:
-        return cls.model_validate(data)
