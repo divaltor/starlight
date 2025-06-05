0c02eb10eec611760683015f225b649d96708a9b
Vlad Vladov <divaltor@pm.me>
2025-06-06 00:19:07 +0200

    Some changes to commands

--- backend/src/handlers/image.ts
+++ backend/src/handlers/image.ts
@@ -1,107 +1,70 @@
 import env from "@/config";
 import { imagesQueue } from "@/queue/image-collector";
 import { scrapperQueue } from "@/queue/scrapper";
-import { Cookies, prisma, redis } from "@/storage";
+import { redis } from "@/storage";
 import type { UserContext } from "@/types";
-import { Scraper, type Tweet } from "@the-convocation/twitter-scraper";
-import { sleep } from "bun";
-import { Composer, InlineKeyboard, InputMediaBuilder } from "grammy";
-import type { InputMediaPhoto } from "grammy/types";
+import type { Tweet } from "@the-convocation/twitter-scraper";
+import { Composer, InlineKeyboard } from "grammy";
 
 const composer = new Composer<UserContext>();
 
 const feature = composer.chatType("private");
 
-feature.command("queue").filter(async (ctx) => {
-	const scheduledJob = await scrapperQueue.getJobScheduler(
-		`scrapper-${ctx.user.id}`,
-	);
-
-	return scheduledJob === null && ctx.session.cookies !== null;
-});
-
 feature.command("queue").filter(
-	(ctx) => ctx.session.cookies !== null,
 	async (ctx) => {
-		const scrapper = new Scraper();
-
-		// biome-ignore lint/style/noNonNullAssertion: <explanation>
-		const cookies = Cookies.fromJSON(ctx.session.cookies!);
-
-		// https://github.com/the-convocation/twitter-scraper/issues/110
-		await scrapper.setCookies(cookies.toString().split(";"));
-
-		// biome-ignore lint/style/noNonNullAssertion: cookies is not null from filter
-		const twitterUserId = cookies!.userId();
-		const telegramUserId = ctx.from.id;
-
-		if (!twitterUserId) {
-			ctx.logger.warn("Could not extract user ID from cookies");
-			await ctx.reply("Could not extract user ID from cookies");
-			return;
-		}
-
-		ctx.logger.debug(
-			{ userId: twitterUserId },
-			"User ID extracted from cookies",
+		const scheduledJob = await scrapperQueue.getJobScheduler(
+			`scrapper-${ctx.user.id}`,
 		);
-		ctx.logger.info("Starting to get liked tweets for user %s", twitterUserId);
 
-		const likedTweets = await scrapper.fetchLikedTweets(twitterUserId, 200);
+		return scheduledJob === null && ctx.session.cookies !== null;
+	},
+	async (ctx) => {
+		ctx.logger.info("Upserting job scheduler for user %s", ctx.user.id);
 
-		for (const tweet of likedTweets.tweets) {
-			imagesQueue.add(
-				"post",
-				{ tweet, userId: ctx.user.id },
-				{
-					deduplication: {
-						id: tweet.id
-							? `${telegramUserId}:${tweet.id}`
-							: `${telegramUserId}:${crypto.randomUUID()}`,
-						ttl: 1000 * 60 * 60 * 24, // 1 day
-					},
+		await scrapperQueue.upsertJobScheduler(
+			`scrapper-${ctx.user.id}`,
+			{
+				every: 1000 * 60 * 60 * 6, // 6 hours
+			},
+			{
+				data: {
+					userId: ctx.user.id,
 				},
-			);
-			ctx.logger.debug({ tweetId: tweet.id }, "Added tweet to queue");
-		}
+			},
+		);
+
+		ctx.logger.info("Job scheduler upserted for user %s", ctx.user.id);
 	},
 );
 
-feature.command("queue", async (ctx) => {
-	const keyboard = new InlineKeyboard().webApp("Set cookies", {
-		url: `${env.BASE_FRONTEND_URL}/cookies`,
-	});
+feature.command("queue").filter(
+	async (ctx) => ctx.session.cookies === null,
+	async (ctx) => {
+		const keyboard = new InlineKeyboard().webApp("Set cookies", {
+			url: `${env.BASE_FRONTEND_URL}/cookies`,
+		});
 
-	await ctx.reply(
-		"Beep boop, you need to give me your cookies before I can send you daily images.",
-		{ reply_markup: keyboard },
-	);
-});
+		await ctx.reply(
+			"Beep boop, you need to give me your cookies before I can send you daily images.",
+			{ reply_markup: keyboard },
+		);
+	},
+);
 
-feature.command("images", async (ctx) => {
-	const buffer: InputMediaPhoto[] = [];
+feature.command("test", async (ctx) => {
+	const tweet = await redis.get("tweet:test");
 
-	const photos = await prisma.photo.findMany({
-		where: {
-			userId: ctx.user.id,
-			s3Path: { not: null },
-		},
-	});
-
-	for (const photo of photos) {
-		// It won't trigger `originalUrl`, just for linter
-		buffer.push(InputMediaBuilder.photo(photo.s3Url ?? photo.originalUrl));
-
-		if (buffer.length === 10) {
-			await ctx.replyWithMediaGroup(buffer);
-			buffer.length = 0;
-			await sleep(1000);
-		}
+	if (!tweet) {
+		await ctx.reply("No tweet found");
+		return;
 	}
 
-	if (buffer.length > 0) {
-		await ctx.replyWithMediaGroup(buffer);
-	}
+	const photos = JSON.parse(tweet) as Tweet;
+
+	imagesQueue.add("post", {
+		tweet: photos,
+		userId: ctx.user.id,
+	});
 });
 
 export default composer;
