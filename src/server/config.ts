dc5fe3626f3b2b7b0bf93e7412bd04ccf885e675
Vlad Vladov <divaltor@pm.me>
2025-06-06 13:38:02 +0200

    Add logging to Axiom

--- src/server/config.ts
+++ src/server/config.ts
@@ -6,18 +6,26 @@ dotenv.config({ path: ".env" });
 const envSchema = z.object({
 	BOT_TOKEN: z.string(),
 	ENVIRONMENT: z.enum(["dev", "prod"]).optional().default("dev"),
+
 	REDIS_URL: z.url({ protocol: /^rediss?$/ }),
 	DATABASE_URL: z.url({ protocol: /^postgresql$/ }),
+
 	LOG_LEVEL: z
 		.enum(["debug", "info", "warn", "error"])
 		.optional()
 		.default("debug"),
+
 	YOUTUBE_DL_PATH: z.string().optional().default("yt-dlp"),
+
 	AWS_ACCESS_KEY_ID: z.string(),
 	AWS_SECRET_ACCESS_KEY: z.string(),
 	AWS_ENDPOINT: z.string().optional(),
+
 	BASE_CDN_URL: z.string(),
 	BASE_FRONTEND_URL: z.string(),
+
+	AXIOM_DATASET: z.string().optional(),
+	AXIOM_TOKEN: z.string().optional(),
 });
 
 const env = envSchema.parse(process.env);
--- src/server/logger.ts
+++ src/server/logger.ts
@@ -3,19 +3,20 @@ import pino from "pino";
 
 export const logger = pino({
 	level: env.LOG_LEVEL,
-	// transport: {
-	// 	targets: [
-	// 		{
-	// 			target: "pino-pretty",
-	// 			level: env.LOG_LEVEL,
-	// 			options: {
-	// 				ignore: "pid,hostname",
-	// 				colorize: true,
-	// 				translateTime: true,
-	// 			},
-	// 		},
-	// 	],
-	// },
+	transport: {
+		targets:
+			env.AXIOM_DATASET && env.AXIOM_TOKEN && env.ENVIRONMENT === "prod"
+				? [
+						{
+							target: "@axiomhq/pino",
+							options: {
+								dataset: env.AXIOM_DATASET,
+								token: env.AXIOM_TOKEN,
+							},
+						},
+					]
+				: [],
+	},
 });
 
 export type Logger = typeof logger;
