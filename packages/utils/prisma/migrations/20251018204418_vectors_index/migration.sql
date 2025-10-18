f4ebeace86e9b0e7cfd3a1c679205675d96c1157
Vlad Vladov <divaltor@pm.me>
2025-10-19 00:21:17 +0300

    Update migration

--- packages/utils/prisma/migrations/20251018204418_vectors_index/migration.sql
+++ packages/utils/prisma/migrations/20251018204418_vectors_index/migration.sql
@@ -1,3 +1,3 @@
 -- This is an empty migration.
-CREATE INDEX IF NOT EXISTS "photos_image_vec_idx" ON "photos" USING hnsw (image_vec);
-CREATE INDEX IF NOT EXISTS "photos_tag_vec_idx" ON "photos" USING hnsw (tag_vec);
+CREATE INDEX IF NOT EXISTS "photos_image_vec_idx" ON "photos" USING hnsw (image_vec vector_cosine_ops);
+CREATE INDEX IF NOT EXISTS "photos_tag_vec_idx" ON "photos" USING hnsw (tag_vec vector_cosine_ops);
