5dceeb185ef15bbb9828f204a36c7836a5a376e3
Vlad Vladov <divaltor@pm.me>
2025-12-16 00:04:01 +0200

    Load env file for Prisma

--- packages/utils/prisma.config.ts
+++ packages/utils/prisma.config.ts
@@ -1,6 +1,11 @@
 // import "dotenv/config"; uncomment this to load .env
 import path from "node:path";
+import dotenv from "dotenv";
 import type { PrismaConfig } from "prisma/config";
+
+dotenv.config({
+	path: [".env", "../../apps/server/.env"],
+});
 
 export default {
 	schema: path.join("prisma", "schema.prisma"),
