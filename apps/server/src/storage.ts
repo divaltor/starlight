c60d8a38b921c3155eabe0df81544c6304c57426
Vlad Vladov <divaltor@pm.me>
2026-05-30 18:43:59 +0300

    Remove dead code

--- apps/server/src/storage.ts
+++ apps/server/src/storage.ts
@@ -1,5 +1,4 @@
 import { env } from "@starlight/utils";
-import type { StorageAdapter } from "grammy";
 import Redis from "ioredis";
 import { Cookie } from "tough-cookie";
 
@@ -8,67 +7,6 @@ export const redis = new Redis(env.REDIS_URL, {
 	enableReadyCheck: true,
 	maxRetriesPerRequest: null,
 });
-
-export class RedisAdapter<T> implements StorageAdapter<T> {
-	readonly redis: Redis;
-	private readonly ttl?: number;
-	private readonly parseJSON?: boolean;
-	/**
-	 * @constructor
-	 * @param {opts} Constructor options
-	 * @param {opts.ttl} ttl - Session time to life in SECONDS.
-	 * @param {opts.instance} instance - Instance of redis.
-	 * @param {opts.parseJSON} parseJSON - Set to true to parse JSON.
-	 */
-	constructor({
-		instance,
-		ttl,
-		parseJSON,
-	}: {
-		instance?: Redis;
-		ttl?: number;
-		parseJSON?: boolean;
-	}) {
-		if (instance) {
-			this.redis = instance;
-		} else {
-			throw new Error("You should pass redis instance to constructor.");
-		}
-
-		this.ttl = ttl;
-		this.parseJSON = parseJSON;
-	}
-
-	async read(key: string) {
-		const session = await this.redis.get(key);
-
-		if (session === null || session === undefined) {
-			return;
-		}
-
-		if (this.parseJSON) {
-			return JSON.parse(session) as unknown as T;
-		}
-
-		return session as unknown as T;
-	}
-
-	async write(key: string, value: T) {
-		if (this.parseJSON) {
-			await this.redis.set(key, JSON.stringify(value));
-		} else {
-			await this.redis.set(key, value as string);
-		}
-
-		if (this.ttl) {
-			this.redis.expire(key, this.ttl);
-		}
-	}
-
-	async delete(key: string) {
-		await this.redis.del(key);
-	}
-}
 
 export interface RFC6265Cookie {
 	domain: string;
@@ -109,10 +47,6 @@ export class Cookies {
 	}
 }
 
-interface SessionData {
-	cookies: RFC6265Cookie[] | null;
-}
-
 export const s3 = new Bun.S3Client({
 	accessKeyId: env.AWS_ACCESS_KEY_ID,
 	secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
@@ -131,5 +65,3 @@ export function mapToRFC6265Cookie(firefoxCookie: any): RFC6265Cookie {
 		domain: extractDomain(firefoxCookie["Host raw"]),
 	};
 }
-
-export type { SessionData };
